0ca10baca609fb826deffeee0514fa55c03acf96
Pieter-Deconinck <pieter.deconinck@student.hogent.be>
2022-07-02 23:56:55 +0200

    Added dot check for filename

--- wemasu_backend/file.js
+++ wemasu_backend/file.js
@@ -2,70 +2,75 @@ const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 class File {
-    #uploadDate;
-    #hours;
-    #expirationDate;
-    #author;
-    #uploadPath;
-    #fileName;
-    #fileSize;
-    #hashedFileName;
+  #uploadDate;
+  #hours;
+  #expirationDate;
+  #author;
+  #uploadPath;
+  #fileName;
+  #fileSize;
+  #hashedFileName;
 
-    constructor(uploadDate, hours = 24, author, uploadPath, fileName, fileSize) {
-        this.#uploadDate = uploadDate;
-        this.#hours = hours;
-        this.#expirationDate = new Date(uploadDate.getTime() + this.#hours * 60 * 60 * 1000);
-        this.#author = author;
-        this.#uploadPath = uploadPath;
-        this.#fileName = fileName;
-        this.#fileSize = fileSize;
-        this.#hashedFileName = bcrypt.hashSync(this.#fileName, parseInt(process.env.FILE_SALT));
-    }
+  constructor(uploadDate, hours = 24, author, uploadPath, fileName, fileSize) {
+    this.#uploadDate = uploadDate;
+    this.#hours = hours;
+    this.#expirationDate = new Date(uploadDate.getTime() + this.#hours * 60 * 60 * 1000);
+    this.#author = author;
+    this.#uploadPath = uploadPath;
+    this.fileName = fileName;
+    this.#fileSize = fileSize;
+    this.#hashedFileName = bcrypt.hashSync(this.#fileName, parseInt(process.env.FILE_SALT));
+  }
 
-    get uploadDate() {
-        return this.#uploadDate;
-    }
+  get uploadDate() {
+    return this.#uploadDate;
+  }
 
-    get hours() {
-        return this.#hours;
-    }
+  get hours() {
+    return this.#hours;
+  }
 
-    get expirationDate() {
-        return this.#expirationDate;
-    }
+  get expirationDate() {
+    return this.#expirationDate;
+  }
 
-    get author() {
-        return this.#author;
-    }
+  get author() {
+    return this.#author;
+  }
 
-    get uploadPath() {
-        return this.#uploadPath;
-    }
+  get uploadPath() {
+    return this.#uploadPath;
+  }
 
-    get fileName() {
-        return this.#fileName;
-    }
+  get fileName() {
+    return this.#fileName;
+  }
 
-    get fileSize() {
-        return this.#fileSize;
-    }
+  get fileSize() {
+    return this.#fileSize;
+  }
 
-    get hashedFileName() {
-        return this.#hashedFileName;
-    }
+  get hashedFileName() {
+    return this.#hashedFileName;
+  }
 
-    toJSON() {
-        return {
-            author: this.#author,
-            uploadDate: this.#uploadDate,
-            hours: this.#hours,
-            expirationDate: this.#expirationDate,
-            uploadPath: this.#uploadPath,
-            fileName: this.#fileName,
-            fileSize: this.#fileSize,
-            hashedFileName: this.#hashedFileName,
-        };
-    }
+  set fileName(name) {
+    if (name[0] === `.`) throw new Error(`File name can't start with a dot: ${name}`);
+    this.#fileName = name;
+  }
+
+  toJSON() {
+    return {
+      author: this.#author,
+      uploadDate: this.#uploadDate,
+      hours: this.#hours,
+      expirationDate: this.#expirationDate,
+      uploadPath: this.#uploadPath,
+      fileName: this.#fileName,
+      fileSize: this.#fileSize,
+      hashedFileName: this.#hashedFileName,
+    };
+  }
 }
 
 module.exports = File; // export class
